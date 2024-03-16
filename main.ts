0683f1066bfa75f0e68f70f242dd4eb9ec1db28c
palorder <138593088+palorder@users.noreply.github.com>
2024-03-16 16:33:22 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,49 @@
 input.onButtonPressed(Button.A, function () {
-    JoyCar.drive(FRDirection.Forward, 50)
-    JoyCar.drive(FRDirection.Reverse, 50)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Left,
+    50,
+    5
+    )
 })
 input.onButtonPressed(Button.AB, function () {
     JoyCar.drive(FRDirection.Forward, 100)
 })
 input.onButtonPressed(Button.B, function () {
-    JoyCar.drive(FRDirection.Reverse, 50)
-    JoyCar.drive(FRDirection.Forward, 50)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Right,
+    50,
+    5
+    )
 })
 basic.forever(function () {
-	
+    // Start collision detection
+    if (JoyCar.collisionDetection() == 1) {
+        JoyCar.stop(StopIntensity.Soft)
+    }
+    // Obstacle avoidance
+    if (JoyCar.obstacleavoidance(SensorLRSelection.Left)) {
+        JoyCar.stop(StopIntensity.Soft)
+        // Pause for a moment before attempting to dodge
+        basic.pause(500)
+        JoyCar.turn(
+        FRDirection.Forward,
+        LRDirection.Right,
+        50,
+        5
+        )
+    }
+    // Obstacle avoidance on the right side
+    if (JoyCar.obstacleavoidance(SensorLRSelection.Right)) {
+        JoyCar.stop(StopIntensity.Intense)
+        // Pause for a moment before attempting to dodge
+        basic.pause(500)
+        JoyCar.turn(
+        FRDirection.Forward,
+        LRDirection.Left,
+        50,
+        5
+        )
+    }
 })
